3c998bd429d48efa9dde9d8191e8a6af24d5549b
Prakhar Gupta <58927877+prakharcr704@users.noreply.github.com>
2020-06-22 16:31:28 +0530

    adding cartScreen

---
+++ frontend/src/screens/CartScreen.js
@@ -0,0 +1,22 @@
+  
+import React, { useEffect } from 'react';
+import { addToCart, removeFromCart } from '../actions/cartActions';
+import { useDispatch, useSelector } from 'react-redux';
+
+import { Link } from 'react-router-dom';
+
+function CartScreen(props)
+{
+    const productId = props.match.params.id;
+    const qty = props.location.search ? Number(props.location.search.split("=")[1]) : 1;
+    const dispatch = useDispatch();
+    useEffect(() => {
+        if (productId) {
+          dispatch(addToCart(productId, qty));
+        }
+      }, []);
+    return <div >
+        Cart Screen
+    </div>
+}
+export default CartScreen;
